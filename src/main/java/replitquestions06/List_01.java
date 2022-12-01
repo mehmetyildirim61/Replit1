199cee46277b8df88f6f3a17d0cde361f9bc9f63
Mehmet Yildirim <mehmetyildrm611@gmail.com>
2022-12-01 21:31:08 +0300

    push the changes

--- src/main/java/replitquestions06/List_01.java
+++ src/main/java/replitquestions06/List_01.java
@@ -1,7 +1,7 @@
 package replitquestions06;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class List_01 {
     //Write a Java program to get a String from user as input and find the maximum occurring character in that string. (Ignore case sensitivity)
@@ -14,26 +14,35 @@ public class List_01 {
     //
     //maximum occurring character is : a
 
-//    public static void main(String[] args) {
-//        String str ="Learning java is easy";
-//        str=str.replaceAll(" ", "");
-//
-//        List<Character> harfler = new ArrayList<>();
-//
-//        for(int i=0; i<str.length(); i++){
-//           if
-//            harfler.add(str.charAt(i));
-//
-//        }
-//
-//        int counter =0;
-//        for(int w: harfler){
-//
-//            if(harfler.indexOf(w))
-//        }
+    public static void main(String[] args) {
+        String str = "Learning java is easy";
+        str = str.replaceAll(" ", "");
 
+        Map<String, Integer> harfler = new HashMap<>();
 
+        String [] arr =str.split("");
 
+        for (String w: arr) {
+            Integer gorunumSayisi=harfler.get(w);
+
+            if(gorunumSayisi==null){
+                harfler.put(w, 1);
+            }else{
+                harfler.put(w, gorunumSayisi+1);
+            }
+        }
+        System.out.println(harfler);
+        int max=0;
+        char maxOccuringChar;
+        for(int w:harfler.values()){
+            max= Math.max(max,w);
+        }
+        for(String w: harfler.keySet()){
+            if(harfler.get(w).intValue()==max){
+                System.out.println("Maximum occuring character: "+w);
+            }
+        }
+    }
     }
 
 
