b80d99dc9b1942eb9b0903bbfbd8c6775b0ab386
Mehmet Yildirim <mehmetyildrm611@gmail.com>
2022-12-03 18:03:35 +0300

    push the changes

--- src/main/java/replitquestions06/List_07.java
+++ src/main/java/replitquestions06/List_07.java
@@ -11,22 +11,20 @@ public class List_07 {
     //
     //Output : [1,2,3,4,5]
     public static void main(String[] args) {
-        int [] arr ={1,2,3,4,5,6};
+        int[] arr = {1, 2, 3, 4, 5, 6};
 
-//        int idx=0;
-//        int length =1;
-//        int [] newArr=new int[length];
-        int removeElement=6;
+        int idx = 0;
+        int length = 1;
+        int[] newArr = new int[5];
+        int removeElement = 4;
 
-//        for(int w:arr){
-//            if(w!=removeElement){
-//                newArr[idx]=w;
-//                idx++;
-//                length++;
+        for (int w : arr) {
+            if (w != removeElement) {
+                newArr[idx] = w;
+                idx++;
             }
         }
+        System.out.println(Arrays.toString(newArr));
 
-//        System.out.println(idx);
-//        System.out.println(Arrays.toString(newArr));
-
-
+    }
+}
--- src/main/java/replitquestions06/List_08.java
+++ src/main/java/replitquestions06/List_08.java
@@ -1,5 +1,7 @@
 package replitquestions06;
 
+import java.util.Scanner;
+
 public class List_08 {
     //Write a java program which accept a sentence as parameter, than reverses sentence by using StringBuilder and checks if sentence is palindrome or not (without case sensitivity).Use StringBuilder.
     //
@@ -12,4 +14,21 @@ public class List_08 {
     //
     //Reversed sentence : avaJ evol I.
     //It is not a palindrome"
+
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Enter a sentence to check if it's a palindrome");
+        StringBuilder str = new StringBuilder(scan.nextLine());
+        String naturelStr= str.toString();
+
+        String reverseStr =str.reverse().toString();
+
+        System.out.println(reverseStr);
+        System.out.println(naturelStr);
+        if(naturelStr.equalsIgnoreCase(reverseStr)){
+            System.out.println("it's a palindrome");
+        }else{
+            System.out.println("it's NOT a palindrome");
+        }
+    }
 }
